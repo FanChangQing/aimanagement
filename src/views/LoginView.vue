57bf3b92cfc16330f8db12584140b1aa233760ff
FanChangQing <1742194960@qq.com>
2023-04-02 23:36:48 +0800

    login+tableView

---
+++ src/views/LoginView.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="loginview">
+    <div class="loginBg">
+      <div class="loginWindow">
+        <div class="view">
+            <el-row style="height:100px;margin: 10px 0px;">
+                <el-col>
+                    <img src="https://img.js.design/assets/img/6242ad7767aa591c03cce0e1.jpg#76d68444a73f65fe9efe67904a8a1c00"/>
+                </el-col>
+            </el-row>
+            <el-row class="title_font">
+                <div class="font_1">
+                    云龄
+                </div>
+                <div class="font_2">
+                    ®
+                </div>
+                <div class="font_3">
+                    AI后台
+                </div>
+            </el-row>
+            <el-form :model="ruleForm" status-icon :rules="loginRules" ref="ruleForm" style="padding-left: 4%;">
+                <div style="margin-bottom: 10px;font-size: 20px;">
+                    账号
+                </div>
+                <el-form-item prop="username"  style="width: 90%;margin-bottom: 25px;">
+                    <el-input
+                        v-model="ruleForm.username"
+                        placeholder="请输入用户名"
+                        autocomplete="off"
+                    ></el-input>
+                </el-form-item>
+                <div style="margin-bottom: 10px;font-size: 20px;">
+                    密码
+                </div>
+                <el-form-item prop="password" style="width: 90%;">
+                    <el-input
+                        type="password"
+                        v-model="ruleForm.password"
+                        placeholder="请输入密码"
+                        autocomplete="off"
+                    ></el-input>
+                </el-form-item>
+                <el-button type="primary" class="loginBt" size="large" @click="login()"
+                    >登录</el-button
+                >
+          </el-form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginView',
+  data () {
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('用户名不能为空'));
+      } else {
+        callback();
+      }
+    };
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('密码不能为空'));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [{ validator: validatePass, trigger: 'blur' }],
+        password: [{ validator: validatePass2, trigger: 'blur' }]
+      },
+      isCheck: false
+    };
+  },
+  methods: {
+    login () {
+      if (this.ruleForm.username === 'admin' && this.ruleForm.password === '123456')
+      {
+        console.log('我点击了')
+        this.$router.push({
+            path:'/TableView',
+        })
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.loginview {
+  width: 100vw;
+  height: 100vh;
+    ::v-deep .el-input__inner
+    {
+        height: 50px !important;
+        font-size: 17px;
+    }
+  .loginBg {
+    width: 100%;
+    height: 100%;
+    background: url(https://img.js.design/assets/img/64240053e8745177e5c7f63a.jpg#990591fb3687e4ae4d7ec3785e032f86);
+    display: flex;
+    align-items: center;
+    background-size: 100% 100%;
+    .loginWindow {
+      position: absolute;
+      width: 400px;
+      height: 500px;
+      left: 10%;
+      background: #fff;
+      box-shadow: 0 2px 6px 0 rgb(0 0 0 / 10%);
+      border-radius: 20px;
+      .view
+      {
+        padding-left:10%;
+        text-align: left;
+        .title_font
+        {
+            line-height: 0;
+            padding-left: 4%;
+            display: flex;
+            height: 45px;
+            opacity: 1;
+            .font_1
+            {
+                /** 文本1 */
+                width: 100px;
+                font-size: 45px;
+                font-weight: 700;
+                letter-spacing: 4px;
+                color: rgba(8, 161, 221, 1);
+            }
+            .font_2
+            {
+                /** 文本2 */
+                width: 30px;
+                // padding-top: 10px;
+                margin-top: -10px;
+                font-size: 40px;
+                font-weight: 400;
+                letter-spacing: 4px;
+                color: rgba(8, 161, 221, 1);
+            }
+            .font_3
+            {
+                /** 文本3 */
+                font-size: 45px;
+                font-weight: 700;
+                letter-spacing: 4px;
+                color: rgba(8, 161, 221, 1);
+                text-align: left;
+                vertical-align: top;
+            }
+        }
+        .loginBt
+        {
+            width: 200px;
+            height: 55px;
+            font-size: 20px;
+            margin: 25px 50px 50px 50px;
+        }
+      }
+
+      .view img
+      {
+        text-align: left;
+        width: 100px;
+        height:100px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/TableView.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="TableView">
+    <div class="title">
+      <div class="title_font">
+        <el-row style="margin-top: 20px">
+          <el-col>
+            <img
+              class="yunling"
+              src="https://img.js.design/assets/img/6242ad7767aa591c03cce0e1.jpg#76d68444a73f65fe9efe67904a8a1c00"
+            />
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col class="title_text"> 云龄®AI系统训练记录 </el-col>
+        </el-row>
+        <el-row>
+          <el-col style="color: #fff; font-size: 25px">
+            Yunling AI System Training Record
+          </el-col>
+        </el-row>
+      </div>
+      <img
+        src="https://img.js.design/assets/img/63228d850e0d177adb89a185.png#de78093b9866204df74ff65e81032bdd"
+      />
+    </div>
+    <div class="table">
+      <el-table :data="tableData" style="width: 100%" :cell-style="{'text-align':'center'}" :header-cell-style="{'text-align':'center','color':'#000'}">
+        <el-table-column prop="date" label="训练时间"  width="180">
+        </el-table-column>
+        <el-table-column prop="type" label="bots类型" width="180">
+        </el-table-column>
+        <el-table-column prop="content" label="训练内容" width="280">
+        </el-table-column>
+        <el-table-column prop="trainer" label="训练者" width="180">
+        </el-table-column>
+        <el-table-column label="操作">
+            <template #default="scope">
+                <el-button type="primary" class="table_button" icon="el-icon-view" @click="search(scope.row,scope.$index)">查看训练材料</el-button>
+                <el-button type="primary" class="table_button" v-if="scope.row.isDelete==false" icon="el-icon-delete" @click="deleteRow(scope.row,scope.$index)">删除训练记录</el-button>
+                <el-button type="info" class="table_button" v-show="scope.row.isDelete==true" plain icon="el-icon-delete">已删除</el-button>
+            </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          type:'客服',
+          trainer: "王小虎",
+          content: "上海市普陀区金沙江路 1518 弄",
+          isDelete:false
+        },
+        {
+        date: "2016-05-02",
+          type:'客服',
+          trainer: "王小虎",
+          content: "上海市普陀区金沙江路 1518 弄",
+          isDelete:false
+        },
+        {
+          date: "2016-05-02",
+          type:'客服',
+          trainer: "王小虎",
+          content: "上海市普陀区金沙江路 1518 弄",
+          isDelete:false
+        },
+        {
+          date: "2016-05-02",
+          type:'客服',
+          trainer: "王小虎",
+          content: "上海市普陀区金沙江路 1518 弄",
+          isDelete:false
+        },
+        {
+          date: "2016-05-02",
+          type:'客服',
+          trainer: "王小虎",
+          content: "上海市普陀区金沙江路 1518 弄",
+          isDelete:false
+        },
+        {
+          date: "2016-05-02",
+          type:'客服',
+          trainer: "王小虎",
+          content: "上海市普陀区金沙江路 1518 弄",
+          isDelete:false
+        },
+      ],
+    };
+  },
+  methods:{
+    search(row,index)
+    {
+        console.log(row)
+    },
+    deleteRow(row,index)
+    {
+        this.tableData[index].isDelete=true
+        console.log(row)
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.TableView {
+  .title {
+    height: 230px;
+    background: linear-gradient(
+      136.56deg,
+      rgba(33, 97, 207, 1) 0%,
+      rgba(8, 161, 221, 1) 100%
+    );
+    overflow: hidden;
+    display: flex;
+    .title_font {
+      width: 65%;
+      text-align: right;
+      .title_text {
+        font-size: 50px;
+        color: #fff;
+        font-weight: 700;
+        letter-spacing: 4px;
+      }
+    }
+    .yunling {
+      width: 80px;
+      height: 80px;
+    }
+  }
+  .title img {
+    height: 300px;
+    width: 300px;
+  }
+  .table
+  {
+    width: 80%;
+    text-align: center;
+    border-radius: 10px;
+    background: rgba(255, 255, 255, 1);
+    box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.1);
+    margin: 10px 10%;
+    .table_button
+    {
+        width: 130px;
+        font-size:10px
+    }
+  }
+}
+</style>
